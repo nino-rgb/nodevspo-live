5ab2d0250357d486e5aa003c6326b99b472d4556
hajime.takeuchi <shadowthe8156@gmail.com>
2025-02-15 17:09:03 +0900

    fix charset utf8 to utf8mb4

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,6 @@ async function main() {
   dotenv.config();
   const { PORT, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
-  console.log(DB_USER);
-  console.log(DB_PASS);
   const app: Express = express();
 
   app.use(express.json());
@@ -30,25 +28,13 @@ async function main() {
     console.log("Node.js is listening to PORT:" + address.port);
   });
 
-  try {
-    const connection = await mysql.createConnection({
-      host: "127.0.0.1" as string,
-      port: parseInt(DB_PORT as string),
-      user: DB_USER as string,
-      password: DB_PASS as string,
-      database: DB_NAME as string,
-    });
-  } catch (e) {
-    console.log(e);
-  }
-
-  // const connection = await mysql.createConnection({
-  //   host: DB_HOST as string,
-  //   port: parseInt(DB_PORT as string),
-  //   user: DB_USER as string,
-  //   password: DB_PASS as string,
-  //   database: DB_NAME as string,
-  // });
+  const connection = await mysql.createConnection({
+    host: DB_HOST as string,
+    port: parseInt(DB_PORT as string),
+    user: DB_USER as string,
+    password: DB_PASS as string,
+    database: DB_NAME as string,
+  });
 
   app.get("/", async (req, res) => {
     res.json("テスト");
